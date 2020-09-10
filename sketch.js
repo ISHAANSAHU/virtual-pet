711fa588f8e35eb4001736ea709d669466c99f3b
Ishaan Sahu <64207258+ISHAANSAHU@users.noreply.github.com>
2020-09-10 20:13:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+var dog;
+var happyDog;
+var database;
+var databaseRef;
+var foodInDB
+var foodStockRef;
+
+function preload()
+{
+  //load images here
+  dogImg = loadImage("images/dogImg.png")
+  happyDogImg = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+  createCanvas(500, 500);
+  database=firebase.database()
+  dog =createSprite(250,250,10,10);
+  dog.addImage(dogImg);
+  dog.scale=0.5;
+  foodStockRef=database.ref('food')
+ foodStockRef.on("value",readStock)
+}
+  
+
+function draw() { 
+  background(46,139,87);
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodInDB);
+    dog.addImage(happyDogImg)
+ }
+ else{
+   dog.addImage(dogImg);
+ }
+textSize(20);
+fill("white")
+ text("press up arrow to feed the dog",100,70);
+
+  drawSprites();
+  //add styles here
+
+}
+function writeStock(x){
+  if(x<=0){
+    x=0
+  }
+  else{
+    x=x-1;
+
+  }
+  database.ref('/').update({
+    food:x
+  })
+    
+  
+}
+
+function readStock(data){
+  foodInDB= data.val();
+  console.log(foodInDB);
+}
